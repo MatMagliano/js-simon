0ed51f3b9395136f957097a12f8af6f274fa610e
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-21 15:41:35 +0100

    crazione 5 numeri random

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-alert('Hello!')
+// alert('Hello!')
+
+//------ VARIABLES --------
+var numRandomArray = [];
+
+//    CREATE 5 RANDOM NUMBERS
+
+while (numRandomArray.length < 5) {
+  var numRandom = getRandomNumber(1, 50);
+  numRandomArray.push(numRandom)
+}
+console.log(numRandomArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------- FUNCTIONS -------------
+
+//        GET RANDOM NUMBER
+function getRandomNumber(numMin, numMax) {
+  numMin = Math.ceil(numMin);
+  numMax = Math.floor(numMax);
+  var result = Math.floor(Math.random() * (numMax - numMin + 1)) + numMin;
+  return result
+}
